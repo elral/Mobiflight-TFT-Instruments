737000f7e3bb29463b494b2a09851aae16d9a736
elral <3263285+elral@users.noreply.github.com>
2024-09-25 17:06:50 +0200

    config in flash

--- Instruments/MFCustomDevicesConfig.h
+++ Instruments/MFCustomDevicesConfig.h
@@ -4,8 +4,6 @@
 // in your MFCustomDevice_platformio.ini
 const char CustomDeviceConfig[] PROGMEM =
     {
-        "1.2.Button Flash:"
-        "8.3.4.0.Encoder Flash:"
-        "11.54.5.Analog Input Flash:"
-        "12.7.6.5.1.InputShifter Flash:"
-        "14.12.8.9.10.11.2.Multiplexer Flash:"};
+        "17.Altimeter.34..Altimeter:1.1.Button:"
+        //"17.Airspeed_Indicator.34..AirSpeed:"
+    };
