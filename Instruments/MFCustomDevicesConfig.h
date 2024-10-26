0917d09ed6506db9d82a3a1487c42c0ce2c5e172
elral <3263285+elral@users.noreply.github.com>
2024-10-26 08:35:42 +0200

    add example in flash, disable reading from flash

--- Instruments/MFCustomDevicesConfig.h
+++ Instruments/MFCustomDevicesConfig.h
@@ -4,6 +4,7 @@
 // in your MFCustomDevice_platformio.ini
 const char CustomDeviceConfig[] PROGMEM =
     {
-        "17.Altimeter.34..Altimeter:1.1.Button:"
+        //"17.Altimeter.34..Altimeter:1.1.Button:"
         //"17.Airspeed_Indicator.34..AirSpeed:"
+        "17.StandbyAttitudeModule.34..Stdby Att Mod:"
     };
